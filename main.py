b5b9634b5ac749bf3b8972b0b33dac092c2161e1
ohkandil <o.hossam@nu.edu.eg>
2024-12-15 14:34:30 +0200

    Refactor main execution flow to integrate motion event logging and improve script management

--- main.py
+++ main.py
@@ -1,38 +1,38 @@
-
 import multiprocessing
 import subprocess
 import time
-import gpiozero as GPIO
-from integrations.blynk_integration import start_blynk, motion_triggered
+from history_logging.log_creation import setup_logger, log_motion_event
+from sensors.motion_sensors import arm_motion_sensor
 
 def run_script(script_path):
     subprocess.run(["python3", script_path])
 
 def main():
+    # Setup logger
+    log_file = 'motion_events.log'
+    logger = setup_logger(log_file)
 
-    print("Initializing Blynk...")
-    start_blynk()
     # Step 1: Arm the motion sensor
+    log_motion_event(logger, "Arming the motion sensor...")
     print("Arming the motion sensor...")
-    # arm_motion_sensor()
-    # motion_detector()
-
+    arm_motion_sensor()
 
     # Step 2: Create separate processes for each script
     scripts = [
+        "sensors/motion_sensors.py",
         "notifications/email_notifier.py",
-        "integrations/adafruit_integration.py",
-        "sensors/motion_sensors.py"
+        "integrations/adafruit_integration.py"
     ]
 
     processes = []
 
     for script in scripts:
         # Reset GPIO pins before starting each script
-        # GPIO.cleanup()
+
         
         process = multiprocessing.Process(target=run_script, args=(script,))
         processes.append(process)
+        log_motion_event(logger, f"Starting script: {script}")
         print(f"Starting script: {script}")
         process.start()
         time.sleep(1)  # 1-second delay between starting scripts
